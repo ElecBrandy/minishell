2718053566e455a1e57460d634a17d34cf491a25
yoo040501 <yoo040501@naver.com>
2024-05-17 09:24:31 +0900

    fix header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,12 @@
 /*   By: dongwook <dongwook@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 17:22:33 by dongwook          #+#    #+#             */
-/*   Updated: 2024/05/16 02:48:08 by dongwook         ###   ########.fr       */
+/*   Updated: 2024/05/17 09:24:12 by dongeunk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
 
 # define TRUE 1
 # define FALSE 0
@@ -30,7 +29,7 @@
 # include <errno.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-#include <termios.h>
+# include <termios.h>
 # include "../libft/libft.h"
 
 typedef struct s_node
@@ -65,42 +64,67 @@ typedef struct s_env
 void	minishell(char *str, char **evnp);
 void	sig_handler(int signal);
 
-/* parsing.c */
-char	***parsing(char *av);
-char	**split_flag(char *av, int len, char flag);
-char	*save_in(char *av, t_util *util);
-int		find_other(char *av, int idx);
+/* parsing_delquote.c */
+char	*del_quote(char *av);
+char	**split_space(char *av, int len);
+char	**check_cmd(char **av);
+int		find_cut(char *av);
+void	del_q(char *av, char *str, t_util *u);
 
-/* parsing_in_pipe.c */
-void	is_outfd(char **str, int *i, t_node *node);
-void	is_infd(char **str, int *i, t_node *node);
-char	**find_fd(char **str, t_node *node);
-void	parsing_in_pipe(char *av, t_node *node);
-char	*add_space(char *av);
+/* parsing_dollar_util.c */
+int		find_env(char *av, int *idx, char ***env);
+char	*get_word(char *av, int *idx);
+int		find_dollar(char *av, char ***env);
 
-/* parsing_util.c */
-void	save_in_node(t_node *node, char **cmd);
-int		find_flag(char *av, char flag);
-int		find_next_quote(char *av, int idx, char flag);
-void	util_init(t_util *util);
-// t_node	*make_node(t_node *node); // dongwook
-t_node	*create_node(void);
-void	append_node(t_node **head, t_node *new_node);
+/* parsing_dollar.c */
+char	**check_dollar(char **av, t_env e);
+char	*change_dollar(char *av, char ***env, int env_len);
+char	***make_env(t_env e);
+void	put_word(char *av, char *word, int *idx);
+
+/* paring_fd.c */
+void	new_file(char **str, int *i, t_node *node);
+void	append_file(char **str, int *i, t_node *node);
+void	check_infile(char **str, int *i, t_node *node);
+void	only_open(char **str, int *i);
+void	is_infd(char **str, int *i, t_node *node, t_env e);
 
 /* parsing_free.c */
 void	free_str(char **str);
 void	free_str_three(char ***str);
 void	free_node(t_node *head);
 
-/* paring_fd.c */
-void	new_file(char **str, int *i, t_node *node);
-void	append_file(char **str, int *i, t_node *node);
-void	check_infile(char **str, int *i, t_node *node);
-void	heredoc_infile(char **str, int *i, t_node *node);
-void	heredoc_process(char **str, int *i, t_node *node);
-char    *del_quote(char *av);
-char	**split_space(char *av, int len);
-char	**check_cmd(char **av);
+/* parsing_heredoc.c */
+void	heredoc_infile(char **str, int *i, t_node *node, t_env e);
+void	heredoc_process(char **str, int *i, t_node *node, t_env e);
+char	*heredoc_check_dollar(char *av, t_env e);
+int		heredoc_readline(char *av, char *limiter, t_node *node, t_env e);
+
+/* parsing_in_pipe.c */
+void	parsing_in_pipe(char *av, t_node *node, t_env env);
+char	*add_space(char *av);
+char	**find_fd(char **str, t_node *node, t_env e);
+int		get_flagcnt(char *av);
+void	is_outfd(char **str, int *i, t_node *node);
+
+/* parsing_node.c */
+void	save_in_node(t_node *node, char **cmd, t_env env);
+t_node	*create_node(void);
+void	append_node(t_node **head, t_node *new_node);
+void	find_path(char *cmd, char **env, t_node *node);
+void	get_path(char **path, t_node *node, char *cmd);
+
+/* parsing_util.c */
+int		find_flag(char *av, char flag);
+int		find_next_quote(char *av, int idx, char flag);
+void	util_init(t_util *util);
+int		ft_max(int a, int b);
+
+/* parsing.c */
+char	***parsing(char *av);
+char	**split_flag(char *av, int len, char flag);
+char	*save_in(char *av, t_util *util);
+int		find_other(char *av, int idx);
 
 /* ↓↓↓↓↓ ======== EXEC ======== ↓↓↓↓↓ */
 
