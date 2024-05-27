75295312e4f4d025a6b9f1364b6a1ccb88ba1df8
yoo040501 <yoo040501@naver.com>
2024-05-27 19:20:31 +0900

    process signal finish

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,22 +6,60 @@
 /*   By: dongwook <dongwook@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 18:52:46 by dongwook          #+#    #+#             */
-/*   Updated: 2024/05/27 18:54:36 by dongeunk         ###   ########.fr       */
+/*   Updated: 2024/05/26 20:42:15 by dongwook         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	run_cmd(t_env *head_env, t_node *node, char *home, pid_t pid)
+static void exec_cmd(t_env *head_env, t_node *node);
+static char	*check_path(char **env, char *cmd);
+static char	**split_paths(char **env);
+static char	*make_path(char *cmd, char **path_list);
+
+
+int run_cmd(t_env *head_env, t_node *node, char *home, pid_t pid)
 {
-	char	**tmp;
+	t_env *cur;
+
+	if (is_builtin(node) != 0) // builtin 일 경우
+	{
+		exec_builtin(head_env, node, home, pid);
+		exit(1); // Error? : 정상종료
+	}
+	else // builtin 아니라 일반 함수인 경우
+	{
+		// cur = is_env(head_env, "PATH");
+		// if (cur->value[0] == '\0')
+		// {
+		// 	ft_putstr_fd("minishell: ", 2);
+		// 	ft_putstr_fd(node->cmd[0], 2);
+		// 	ft_putstr_fd(": No such file or directory\n", 2);
+		// 	exit(1); // Error
+		// }
+		exec_cmd(head_env, node); // 환경변수단에 넣어보고 일단 실행
+	}
+	return (0);
+}
+
+// cmd 실행
+static void exec_cmd(t_env *head_env, t_node *node)
+{
+	char 	**tmp;
 
 	if (!head_env)
-		exit (12);
+	{
+		fprintf(stderr, "env is NULL\n");
+		exit(1); // Error
+	}
 	tmp = env_list_to_array(head_env);
 	if (!tmp)
-		exit (12);
+	{
+		fprintf(stderr, "env is NULL\n");
+		exit(1); // Error
+	}
 	if (execve(node->path, node->cmd, tmp) == -1)
-		exit (12);
-	exit (0);
+	{
+		exit(1); // Error
+	}
 }
